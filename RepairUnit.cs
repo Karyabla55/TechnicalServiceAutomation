1c8b6fbd50de2edc2811d7cf71486ba671ff51f7
Sadık Abdusselam Albayrak <s.abdusselam@gmail.com>
2024-04-04 00:16:18 +0300

    Repair Units added

---
+++ RepairUnit.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnicalServiceAutomation
+{
+    public class RepairUnit
+    {
+        public string Id;
+        public int EmployeeCapacity;
+        public Stacks<Packages> WorkCapacity;
+        public LinkList<int> RepairType;
+
+        RepairUnit(string ıd,int employee, int capacity, string repairType) 
+        {
+            Id = ıd;
+            EmployeeCapacity = employee;
+            WorkCapacity = new Stacks<Packages>(capacity);
+            SaveTypes(repairType);
+        }
+
+        private void SaveTypes(string repairType)
+        {
+            string[] chars = repairType.Split('*');
+            foreach(string rT in chars) {
+                RepairType.addToLast(int.Parse(rT));
+            }
+
+        }
+    }
+}
